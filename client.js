e9632c2c2bfc048793bc931e145b1c6fc5c53637
Suchir Balaji <suchirbalaji@gmail.com>
2020-05-05 22:57:16 -0700

    add lobby code

---
+++ client.js
@@ -0,0 +1,51 @@
+const socket = io();
+
+PAGES = ['menu', 'lobby']
+function showPage(page) {
+    PAGES.forEach(x => $(`#${x}`).hide());
+    $(`#${page}`).show();
+}
+showPage('menu');
+
+function getUsername() {
+    const username = $('#username').val().substr(0, 10);
+    if(username === '') {
+        alert('username required');
+        return null;
+    }
+    return username;
+}
+
+function createGame() {
+    const username = getUsername();
+    if(username !== null) {
+        socket.emit('createGame', username);
+    }
+}
+
+function joinGame() {
+    const username = $('#username').val(), lobbyno = $('#lobbyno').val();
+    if(lobbyno === '') {
+        alert('lobby # required');
+    } else if(username !== null) {
+        socket.emit('joinGame', username, Number(lobbyno));
+    }
+}
+
+function playerReady() {
+    $("#ready").prop('disabled', true);
+    socket.emit('playerReady');
+}
+
+socket.on('sendLobby', lobby => {
+    $('#lobbyname').text(`lobby #${lobby.id}`);
+    const playerlist = $('#playerlist');
+    playerlist.empty();
+    for(const player of lobby.players) {
+        const text = `${player.username} (${player.ready? 'ready' : 'not ready'})`
+        playerlist.append(`<li> ${text} </li>`)
+    }
+    showPage('lobby');
+});
+
+socket.on('joinFail', message => alert(message));
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 4242;
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+http.listen(port, () => console.log(`serving on port ${port}`));
+
+const servers = new Map();
+const socketToServer = new Map();
+
+class Server {
+    constructor(id) {
+        this.id = id;
+        this.players = new Map();
+        this.maxPlayers = 4;
+        this.numReady = 0;
+    }
+
+    sendLobby() {
+        const lobby = {
+            id: this.id,
+            players: Array.from(this.players.values())
+        }
+        io.to(this.id).emit('sendLobby', lobby);
+    }
+
+    addPlayer(socket, username) {
+        socket.join(this.id);
+        socketToServer.set(socket.id, this);
+        this.players.set(socket.id, {username, ready: false});
+        this.sendLobby();
+    }
+
+    playerReady(socket) {
+        this.players.get(socket.id).ready = true;
+        this.numReady++;
+        this.sendLobby();
+    }
+
+    disconnect(socket) {
+        this.players.delete(socket.id);
+        this.numReady--;
+        this.sendLobby();
+    }
+
+    full() {
+        return this.players.size == this.maxPlayers;
+    }
+
+    static create() {
+        const id = Math.floor(Math.random()*1e9);
+        const server = new Server(id);
+        servers.set(id, server);
+        return server;    
+    }
+}
+
+
+io.on('connection', (socket) => {
+    socket.on('createGame', username => {
+        server = Server.create();
+        server.addPlayer(socket, username);
+    });
+    socket.on('playerReady', () => {
+        socketToServer.get(socket.id).playerReady(socket);
+    });
+    socket.on('joinGame', (username, lobbyno) => {
+        if(servers.has(lobbyno)) {
+            const server = servers.get(lobbyno);
+            if(server.full()) {
+                socket.emit('joinFail', 'server is full');
+            } else {
+                server.addPlayer(socket, username);
+            }
+        } else {
+            socket.emit('joinFail', 'server not found');
+        }
+    })
+    socket.on('disconnect', () => {
+        socketToServer.get(socket.id).disconnect(socket);
+    })
+})
+
